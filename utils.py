a3b407455657b634661d78701108cd33b99eab96
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2020-04-18 20:06:15 +0800

    Update

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re,requests
+from models.biliVideo import biliVideo
 
 def filenameparser(filename):
     pattern = r'[\\/:*?"<>|\r\n]+'
@@ -23,3 +24,34 @@ def httpPost(url, maxReconn=5, **kwargs):
             trial += 1
             continue
     return None
+
+
+class videoIdConvertor():
+    table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+    tr = dict(("fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"[i], i) for i in range(58))
+    s = [11, 10, 3, 8, 4, 6]
+    xor = 177451812
+    add = 8728348608
+
+    @classmethod
+    def bv2av(cls, x):
+        r = 0
+        for i in range(6):
+            r += cls.tr[x[cls.s[i]]] * 58 ** i
+        return (r - cls.add) ^ cls.xor
+
+    @classmethod
+    def av2bv(cls, x):
+        x = (int(x) ^ cls.xor) + cls.add
+        r = list('BV1  4 1 7  ')
+        for i in range(6):
+            r[cls.s[i]] = cls.table[x // 58 ** i % 58]
+        return ''.join(r)
+
+    @classmethod
+    def urlConvert(cls, url):
+        if re.search(biliVideo.patternBv, url):
+            return biliVideo.videoUrl % ("av%s" % cls.bv2av(re.search(biliVideo.patternBv, url).group()))
+        if re.search(biliVideo.patternAv, url):
+            return biliVideo.videoUrl % cls.av2bv(int(re.search(biliVideo.patternAv, url).group()[2::]))
+        return ""
