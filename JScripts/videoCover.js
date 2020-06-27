29c6a6e56391754f1b4efc97b91e43ad8e6043c3
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2020-06-27 19:17:29 +0800

    add js video downloader

--- JScripts/videoCover.js
+++ JScripts/videoCover.js
@@ -1,3 +1,4 @@
 javascript: (function() {
-	location.href = window.__INITIAL_STATE__.videoData.pic;
+	//location.href = window.__INITIAL_STATE__.videoData.pic;
+	window.open(window.__INITIAL_STATE__.videoData.pic)
 })();
