9206b697721d8135737324c02a1cae568bfdf059
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2020-06-29 18:55:40 +0800

    add js video downloader + bangumi

--- JScripts/videoCover.js
+++ JScripts/videoCover.js
@@ -1,4 +1,12 @@
 javascript: (function() {
 	//location.href = window.__INITIAL_STATE__.videoData.pic;
-	window.open(window.__INITIAL_STATE__.videoData.pic)
+	var picurl;
+	// video
+	if (typeof(window.__INITIAL_STATE__.videoData) != "undefined"){
+		picurl = window.__INITIAL_STATE__.videoData.pic;
+	}else{
+		// bangumi
+		picurl =window.__INITIAL_STATE__.mediaInfo.cover;
+	}
+	window.open(picurl);
 })();
